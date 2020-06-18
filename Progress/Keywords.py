807e81fe23ccfc32497c1bd43bf2fb5e429d06eb
srbhr <srbh077@gmail.com>
2020-06-18 19:40:51 +0530

    Updating Progress

--- Progress/Keywords.py
+++ Progress/Keywords.py
@@ -1,3 +1,9 @@
 import spacy
 from spacy.matcher import Matcher, PhraseMatcher
 import re
+
+nlp = spacy.load('en_core_web_md')
+
+matcher = Matcher(nlp.vocab)  # Matcher object should share the same vocabulary as the whole document
+
+def 
