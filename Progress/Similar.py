688ce8f5afc967c2b558d3b406a8d54c0c183c9b
srbhr <srbh077@gmail.com>
2020-09-13 18:05:21 +0530

    Updating Progress

--- Progress/Similar.py
+++ Progress/Similar.py
@@ -1,2 +1,14 @@
-import textdistance
-import abydos
+import textdistance as td
+import Cleaner
+
+def match(resume, job_des):
+    j = td.jaccard.similarity(resume, job_des)*100
+    s = td.sorensen_dice.similarity(resume, job_des)*100
+    c = td.cosine.similarity(resume, job_des)*100
+    o = td.overlap.normalized_similarity(resume, job_des)*100
+    total = (j+s+c+o)/4
+    return total
+
+# https://realpython.com/working-with-files-in-python/
+
+# https://support.dlink.ca/emulators/wbr2310/index.htm
