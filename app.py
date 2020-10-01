93281ddbda230a599b409db98ea6050cca87e7a3
srbhr <srbh077@gmail.com>
2020-10-02 04:50:36 +0530

    Updated app.py and removed some extra lines

--- app.py
+++ app.py
@@ -17,33 +17,7 @@ import time
 image = Image.open('Images//logo.png')
 st.image(image, use_column_width=True)
 
-st.title("Naive Resume Matcher")
-st.markdown("""
-A Machine Learning Based Resume Matcher, to compare Resumes with Job Descriptions.
-Create a score based on how good/similar a resume is to the particular Job Description.\n
-Documents are sorted based on Their TF-IDF Scores (Term Frequency-Inverse Document Frequency)
-
-Matching Algorihms used are :-
-- **String Matching**
-    - Monge Elkan
-
-- **Token Based**
-    - Jaccard
-    - Cosine
-    - Sorensen-Dice
-    - Overlap Coefficient
-
-Topic Modelling of Resumes is done to provide additional information about the resumes and what clusters/topics,
-the belong to. 
-For this :-
-
-1. LSA (Latenet Semantic Analysis) (aka TruncatedSVD in sklearn) is done after TF-IDF.
-2. id2word, and doc2word algorithms are used on the Documents.
-3. LDA or Latent Dirichlet Allocation is done to extract the Topics from the Document set.(In this case Resumes)
-4. Additional Plots are done to gain more insights about the document.
-
-Total Score calculate is the overall average of the 4 mentioned token based algorithms and string based.
-""")
+st.title("Resume Matcher")
 
 
 # Reading the CSV files prepared by the fileReader.py
