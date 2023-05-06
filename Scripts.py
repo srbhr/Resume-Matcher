f8c169b2c625e4625ee10121b0239f5b0577065e
saurabhraidev <srbh077@gmail.com>
2023-05-06 16:18:15 +0530

    002 WIP for preprocessing

--- Scripts.py
+++ Scripts.py
@@ -6,7 +6,8 @@ def clean_string(text):
     # Remove new lines
     text = text.replace('\n', ' ')
     # remote phone numbers
-    text = re.sub(r'^(\+\d{1,3})?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$', '', text)
+    text = re.sub(
+        r'^(\+\d{1,3})?[-.\s]?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$', '', text)
     phone_pattern = r"\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}"
     text = re.sub(phone_pattern, '', text)
     return text
@@ -31,3 +32,18 @@ def clean_string_nltk(text):
     text = ' '.join(words)
 
     return text
+
+
+def parse_string(text, keywords):
+    data = {}
+    for keyword in keywords:
+        start_index = text.find(keyword)
+        if start_index != -1:
+            end_index = len(text)
+            for next_keyword in keywords:
+                next_index = text.find(
+                    next_keyword, start_index + len(keyword))
+                if next_index != -1 and next_index < end_index:
+                    end_index = next_index
+            data[keyword] = text[start_index + len(keyword):end_index].strip()
+    return data
