b4077df149369e36f411f00eacbe212492e45bb3
Subramanyam Challa <challasubbu25@gmail.com>
2023-08-08 09:07:10 +0530

    resolved conflicts and added logging

--- scripts/similarity/get_similarity_score.py
+++ scripts/similarity/get_similarity_score.py
@@ -1,10 +1,33 @@
 import json
+import logging
 import os
 
 import cohere
 import yaml
 from qdrant_client import QdrantClient, models
 from qdrant_client.http.models import Batch
+
+logging.basicConfig(
+    filename='app_similarity_score.log',
+    filemode='w',
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+console_handler.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler("app_similarity_score.log")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 
 def find_path(folder_name):
@@ -27,9 +50,17 @@ config_path = os.path.join(cwd, "scripts", "similarity")
 
 
 def read_config(filepath):
-    with open(filepath) as f:
-        config = yaml.safe_load(f)
-    return config
+    try:
+        with open(filepath) as f:
+            config = yaml.safe_load(f)
+        return config
+    except FileNotFoundError as e:
+        logger.error(f"Configuration file {filepath} not found: {e}")
+    except yaml.YAMLError as e:
+        logger.error(f"Error parsing YAML in configuration file {filepath}: {e}", exc_info=True)
+    except Exception as e:
+        logger.error(f"Error reading configuration file {filepath}: {e}")
+    return None
 
 
 def read_doc(path):
@@ -37,7 +68,7 @@ def read_doc(path):
         try:
             data = json.load(f)
         except Exception as e:
-            print(f'Error reading JSON file: {e}')
+            logger.error(f'Error reading JSON file: {e}')
             data = {}
     return data
 
@@ -50,7 +81,6 @@ class QdrantSearch:
         self.qdrant_url = config['qdrant']['url']
         self.resumes = resumes
         self.jd = jd
-
         self.cohere = cohere.Client(self.cohere_key)
 
         self.qdrant = QdrantClient(
@@ -67,9 +97,17 @@ class QdrantSearch:
             )
         )
 
+        self.logger = logging.getLogger(self.__class__.__name__)
+
+        self.logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
+
     def get_embedding(self, text):
-        embeddings = self.cohere.embed([text], "large").embeddings
-        return list(map(float, embeddings[0])), len(embeddings[0])
+        try:
+            embeddings = self.cohere.embed([text], "large").embeddings
+            return list(map(float, embeddings[0])), len(embeddings[0])
+        except Exception as e:
+            self.logger.error(f"Error getting embeddings: {e}", exc_info=True)
 
     def update_qdrant(self):
         vectors = []
@@ -78,16 +116,18 @@ class QdrantSearch:
             vector, size = self.get_embedding(resume)
             vectors.append(vector)
             ids.append(i)
+        try:
+            self.qdrant.upsert(
+                collection_name="collection_resume_matcher",
+                points=Batch(
+                    ids=ids,
+                    vectors=vectors,
+                    payloads=[{"text": resume} for resume in self.resumes]
 
-        self.qdrant.upsert(
-            collection_name="collection_resume_matcher",
-            points=Batch(
-                ids=ids,
-                vectors=vectors,
-                payloads=[{"text": resume} for resume in self.resumes]
-
+                )
             )
-        )
+        except Exception as e:
+            self.logger.error(f"Error upserting the vectors to the qdrant collection: {e}", exc_info=True)
 
     def search(self):
         vector, _ = self.get_embedding(self.jd)
@@ -108,10 +148,27 @@ class QdrantSearch:
         return results
 
 
-def get_similarity_score(resume_string, jd_string):
-    qdrant_search = QdrantSearch([resume_string], jd_string)
+def get_similarity_score(resume_string, job_description_string):
+    logger.info("Started getting similarity score")
+    qdrant_search = QdrantSearch([resume_string], job_description_string)
     qdrant_search.update_qdrant()
-    results = qdrant_search.search()
-    return results
+    search_result = qdrant_search.search()
+    logger.info("Finished getting similarity score")
+    return search_result
 
 
+if __name__ == "__main__":
+    # To give your custom resume use this code
+    resume_dict = read_config(
+        READ_RESUME_FROM + "/Resume-bruce_wayne_fullstack.pdf4783d115-e6fc-462e-ae4d-479152884b28.json")
+    job_dict = read_config(
+        READ_JOB_DESCRIPTION_FROM + "/JobDescription-job_desc_full_stack_engineer_pdf4de00846-a4fe-4fe5-a4d7"
+                                    "-2a8a1b9ad020.json")
+    resume_keywords = resume_dict["extracted_keywords"]
+    job_description_keywords = job_dict["extracted_keywords"]
+
+    resume_string = ' '.join(resume_keywords)
+    jd_string = ' '.join(job_description_keywords)
+    final_result = get_similarity_score(resume_string, jd_string)
+    for r in final_result:
+        print(r)
