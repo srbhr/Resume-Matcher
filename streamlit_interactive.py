1987c10c65d1cb87d12895f9f36f3c6d6ae7efc4
imhalcyon <shake.aftermath@gmail.com>
2023-09-16 11:16:01 +0530

    UI: Remove footer links, add Back to Top link

--- streamlit_interactive.py
+++ streamlit_interactive.py
@@ -331,17 +331,5 @@ if os.path.exists(config_file_path):
 else:
     print("Config file does not exist.")
 
-# Display the main title and subheaders again
-st.title(':blue[Resume Matcher]')
-st.subheader(
-    'Free and Open Source ATS to help your resume pass the screening stage.')
-st.markdown(
-    '⭐ Give Resume Matcher a Star on [GitHub](https://github.com/srbhr/Resume-Matcher/)')
-badge(type="github", name="srbhr/Resume-Matcher")
-
-st.text('For updates follow me on Twitter.')
-badge(type="twitter", name="_srbhr_")
-
-st.markdown(
-    'If you like the project and would like to further help in development please consider 👇')
-badge(type="buymeacoffee", name="srbhr")
+# Go back to top
+st.markdown('[:arrow_up: Back to Top](#resume-matcher)')
