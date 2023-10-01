b974c00395af6099093321b0ffe6446144cb601e
Ramon Arambula <r_arambula@u.pacifc.edu>
2023-09-30 22:37:10 -0700

    Created resume client

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import shutil
+from typing import Dict, Union
+
 
 FILE_PATH = os.path.abspath(__file__)
 SUBMODULE_PARENT_DIR = os.path.abspath(os.path.join(FILE_PATH, os.pardir))
@@ -21,56 +24,73 @@ import json
 from scripts import ResumeProcessor
 from scripts.utils import init_logging_config, get_filenames_from_dir
 import logging
-import os
 
 init_logging_config()
 
 PROCESSED_RESUMES_PATH = os.path.join(SUBMODULE_PARENT_DIR, "Data/Processed/Resumes")
 RESUME_INPUT_DIR = os.path.join(SUBMODULE_PARENT_DIR, "Data/Resumes")
 
+class Resume:
+    def __init__(self, file_location: Union[str, os.PathLike]):
+        self.file_location = file_location
+        self.file_name = os.path.basename(self.file_location)
+        self.parsed_data = dict
 
+    def send_to_parser_dir(self) -> None:
+        logging.info(f"original file location: {self.file_location}")
+        shutil.move(self.file_location, RESUME_INPUT_DIR) # moving file to input dir
+        self.file_location = os.path.join(RESUME_INPUT_DIR, self.file_name) # reflecting that file was moved to new location
+        logging.info(f"new file location: {self.file_location}")
 
-def parser() -> None:
-    def read_json(filename):
-        with open(filename) as f:
+    def parser(self) -> dict:
+        def read_json(filename):
+            with open(filename) as f:
+                data = json.load(f)
+            return data
+
+        def remove_old_files(files_path):
+            logging.info("Unhidden files:")
+            logging.info(list_unhidden_files(files_path))
+
+            for filename in list_unhidden_files(files_path):
+                try:
+                    file_path = os.path.join(files_path, filename)
+
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                except Exception as e:
+                    logging.error(f"Error deleting {file_path}:\n{e}")
+
+            logging.info("Deleted old files from " + files_path)
+
+        logging.info("Started to read from Data/Resumes")
+        try:
+            # Check if there are resumes present or not.
+            # If present then parse it.
+            remove_old_files(PROCESSED_RESUMES_PATH)
+
+            input_files = get_filenames_from_dir(RESUME_INPUT_DIR)
+            logging.info("Reading from Data/Resumes is now complete.")
+            logging.info("Unhidden files:")
+            logging.info(list_unhidden_files(RESUME_INPUT_DIR))
+        except:
+            # Exit the program if there are no resumes.
+            logging.error("There are no resumes present in the specified folder.")
+            logging.error("Exiting from the program.")
+            logging.error("Please add resumes in the Data/Resumes folder and try again.")
+            exit(1)
+
+        # Now after getting the file_names parse the resumes into a JSON Format.
+        logging.info("Started parsing the resumes.")
+        for file in input_files:
+            processor = ResumeProcessor(file)
+            success = processor.process()
+            logging.info(f"Parsed: {file}")
+        logging.info("Parsing of the resumes is now complete.")
+        parsed_file:str = os.path.join(PROCESSED_RESUMES_PATH, [f for f in list_unhidden_files(PROCESSED_RESUMES_PATH)][0])
+        logging.info(f"Parsed files: {parsed_file}")
+
+        data = dict
+        with open(parsed_file) as f:
             data = json.load(f)
         return data
-
-    def remove_old_files(files_path):
-        logging.info("Unhidden files:")
-        logging.info(list_unhidden_files(files_path))
-
-        for filename in list_unhidden_files(files_path):
-            try:
-                file_path = os.path.join(files_path, filename)
-
-                if os.path.isfile(file_path):
-                    os.remove(file_path)
-            except Exception as e:
-                logging.error(f"Error deleting {file_path}:\n{e}")
-
-        logging.info("Deleted old files from " + files_path)
-
-    logging.info("Started to read from Data/Resumes")
-    try:
-        # Check if there are resumes present or not.
-        # If present then parse it.
-        remove_old_files(PROCESSED_RESUMES_PATH)
-
-        file_names = get_filenames_from_dir(RESUME_INPUT_DIR)
-        logging.info("Reading from Data/Resumes is now complete.")
-        logging.info("Unhidden files:")
-        logging.info(list_unhidden_files(RESUME_INPUT_DIR))
-    except:
-        # Exit the program if there are no resumes.
-        logging.error("There are no resumes present in the specified folder.")
-        logging.error("Exiting from the program.")
-        logging.error("Please add resumes in the Data/Resumes folder and try again.")
-        exit(1)
-
-    # Now after getting the file_names parse the resumes into a JSON Format.
-    logging.info("Started parsing the resumes.")
-    for file in file_names:
-        processor = ResumeProcessor(file)
-        success = processor.process()
-    logging.info("Parsing of the resumes is now complete.")
