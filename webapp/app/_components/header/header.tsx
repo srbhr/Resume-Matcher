5074669877ae8bcf0aca9be9385940b6d5b28a75
sandip <sandip@pop-os.localdomain>
2023-10-03 15:37:01 +0530

    enhancing landing page: fixed header size change font from inter to fira sans

--- webapp/app/_components/header/header.tsx
+++ webapp/app/_components/header/header.tsx
@@ -1,56 +1,59 @@
-import Image from "next/image";
-import Button from "@/components/button/button";
+import Image from 'next/image';
+import Button from '@/components/button/button';
 
 const Header = () => {
-  return (
-    <header className="flex justify-between items-center px-32 py-4 bg-[#211E27]">
-      <Image
-        src="/resume_matcher_logo.png"
-        alt="Resume Matcher Logo"
-        width={250}
-        height={90}
-      />
-      <nav className="grow">
-        <ul className="flex justify-center gap-6 text-[#DCCDCD]" role="list">
-          <li>Contributions</li>
-          <li>Docs</li>
-          <li>Main Site</li>
-        </ul>
-      </nav>
-      <nav>
-        <ul className="flex gap-6">
-          <li>
-            <a
-              href="https://github.com/srbhr/Resume-Matcher"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button className="bg-white text-black ">
-                Star Us
-                <Image
-                  src="/icons/github.svg"
-                  width={24}
-                  height={24}
-                  alt="GitHub Octocat Logo"
-                />
-              </Button>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://dsc.gg/resume-matcher"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button className="bg-[#302442] text-white">
-                Join our Discord
-              </Button>
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header className="flex justify-between items-center px-24 py-4 bg-[#211E27]">
+            <Image
+                src="/resume_matcher_logo.png"
+                alt="Resume Matcher Logo"
+                width={100}
+                height={60}
+            />
+            <nav className="grow">
+                <ul
+                    className="flex justify-center gap-6 text-[#DCCDCD] text-sm"
+                    role="list"
+                >
+                    <li>Contributions</li>
+                    <li>Docs</li>
+                    <li>Main Site</li>
+                </ul>
+            </nav>
+            <nav>
+                <ul className="flex gap-4 text-xs">
+                    <li>
+                        <a
+                            href="https://github.com/srbhr/Resume-Matcher"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Button className="bg-white text-black py-2">
+                                Star Us
+                                <Image
+                                    src="/icons/github.svg"
+                                    width={16}
+                                    height={16}
+                                    alt="GitHub Octocat Logo"
+                                />
+                            </Button>
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://dsc.gg/resume-matcher"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Button className="bg-[#302442] text-white py-2">
+                                Join our Discord
+                            </Button>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
--- webapp/app/_components/resume/file-upload/file-upload.tsx
+++ webapp/app/_components/resume/file-upload/file-upload.tsx
@@ -1,93 +1,93 @@
-"use client";
+'use client';
 
-import clsx from "clsx";
-import { useState } from "react";
-import { useGlobalStore } from "@/stores/useGlobalStore";
+import clsx from 'clsx';
+import { useState } from 'react';
+import { useGlobalStore } from '@/stores/useGlobalStore';
 
 type FileUploadProps = {
-  buttonLabel?: string;
-  dropZoneLabel?: string;
+    buttonLabel?: string;
+    dropZoneLabel?: string;
 };
 
-const FILE_TYPES = ["application/pdf"];
+const FILE_TYPES = ['application/pdf'];
 
 const FileUpload = ({
-  buttonLabel = "Select File",
-  dropZoneLabel = "Or Drop File Here",
+    buttonLabel = 'Select File',
+    dropZoneLabel = 'Or Drop File Here',
 }: FileUploadProps) => {
-  const { setFile, clearResumeProcessorResponse } = useGlobalStore();
+    const { setFile, clearResumeProcessorResponse } = useGlobalStore();
 
-  const [fileName, setFileName] = useState<string>();
-  const [isDragging, setIsDragging] = useState(false);
+    const [fileName, setFileName] = useState<string>();
+    const [isDragging, setIsDragging] = useState(false);
 
-  const acceptedFileTypes = FILE_TYPES.join(", ");
+    const acceptedFileTypes = FILE_TYPES.join(', ');
 
-  function saveSelectedFileToState(file: File) {
-    setFileName(file.name);
-    setFile(file);
-    clearResumeProcessorResponse();
-  }
-
-  function handleUpload(e: React.ChangeEvent<HTMLInputElement>) {
-    const uploadedFile = e.target.files?.[0] || null;
-
-    if (uploadedFile && FILE_TYPES.includes(uploadedFile.type)) {
-      saveSelectedFileToState(uploadedFile);
+    function saveSelectedFileToState(file: File) {
+        setFileName(file.name);
+        setFile(file);
+        clearResumeProcessorResponse();
     }
-  }
 
-  function handleDrop(e: React.DragEvent<HTMLDivElement>) {
-    e.preventDefault();
-    setIsDragging(false);
+    function handleUpload(e: React.ChangeEvent<HTMLInputElement>) {
+        const uploadedFile = e.target.files?.[0] || null;
 
-    const droppedFile = e.dataTransfer.files?.[0] || null;
-
-    if (droppedFile && FILE_TYPES.includes(droppedFile.type)) {
-      saveSelectedFileToState(droppedFile);
+        if (uploadedFile && FILE_TYPES.includes(uploadedFile.type)) {
+            saveSelectedFileToState(uploadedFile);
+        }
     }
-  }
 
-  function handleDragOver(e: React.DragEvent<HTMLDivElement>) {
-    e.preventDefault();
-    setIsDragging(true);
-  }
+    function handleDrop(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        setIsDragging(false);
 
-  return (
-    <div
-      className={clsx(
-        "flex flex-col gap-4 p-6 border-2 border-dashed border-gray-600 rounded-lg",
-        { "border-purple-100 opacity-50": isDragging }
-      )}
-      onDragOver={handleDragOver}
-      onDragLeave={() => setIsDragging(false)}
-      onDrop={handleDrop}
-    >
-      <input
-        type="file"
-        id="upload-btn"
-        onChange={handleUpload}
-        accept={acceptedFileTypes}
-        hidden
-        multiple={false}
-      />
-      <label
-        htmlFor="upload-btn"
-        className="block text-white text-4xl p-12 rounded-2xl cursor-pointer bg-gradient-to-r from-[#212234] to-[#1A2350]"
-      >
-        {buttonLabel}
-      </label>
-      <small className="text-center text-gray-300">
-        Accepted file type(s): {acceptedFileTypes}
-      </small>
-      {fileName ? (
-        <span className="block text-2xl text-center text-emerald-500">
-          {fileName}
-        </span>
-      ) : (
-        <p className="block text-2xl text-center">{dropZoneLabel}</p>
-      )}
-    </div>
-  );
+        const droppedFile = e.dataTransfer.files?.[0] || null;
+
+        if (droppedFile && FILE_TYPES.includes(droppedFile.type)) {
+            saveSelectedFileToState(droppedFile);
+        }
+    }
+
+    function handleDragOver(e: React.DragEvent<HTMLDivElement>) {
+        e.preventDefault();
+        setIsDragging(true);
+    }
+
+    return (
+        <div
+            className={clsx(
+                'flex flex-col gap-4 p-6 border-2 border-solid border-gray-700 rounded-lg',
+                { 'border-purple-100 opacity-50': isDragging }
+            )}
+            onDragOver={handleDragOver}
+            onDragLeave={() => setIsDragging(false)}
+            onDrop={handleDrop}
+        >
+            <input
+                type="file"
+                id="upload-btn"
+                onChange={handleUpload}
+                accept={acceptedFileTypes}
+                hidden
+                multiple={false}
+            />
+            <label
+                htmlFor="upload-btn"
+                className="block text-white text-3xl p-8 rounded-2xl cursor-pointer bg-gradient-to-r from-[#212234] to-[#1A2350]"
+            >
+                {buttonLabel}
+            </label>
+            <small className="text-center text-gray-300">
+                Accepted file type(s): {acceptedFileTypes}
+            </small>
+            {fileName ? (
+                <span className="block text-2xl text-center text-emerald-500">
+                    {fileName}
+                </span>
+            ) : (
+                <p className="block text-2xl text-center">{dropZoneLabel}</p>
+            )}
+        </div>
+    );
 };
 
 export default FileUpload;
