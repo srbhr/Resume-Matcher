ce3d38ecab079f963b5229565626a2a2f71c0857
sandip <sandip@pop-os.localdomain>
2023-10-21 14:11:17 +0530

    Changes made added byline highlighting the maximum vector score possible darkened vector score color to highlight it added rounded corners to each section added hover state to buttons changed border-dotted to dashed

--- webapp/app/_components/job-descriptions/add-jobs/add-jobs.tsx
+++ webapp/app/_components/job-descriptions/add-jobs/add-jobs.tsx
@@ -65,25 +65,25 @@ const AddJobs = ({ numMaxJobs = 1 }: AddJobsProps) => {
         "link" in job ? JOB_DETAIL_TYPE.LINK : JOB_DETAIL_TYPE.DESCRIPTION;
 
       return (
-        <div key={job.id} className="flex flex-col gap-2 p-4 bg-[#ededed]">
+        <div
+          key={job.id}
+          className="flex flex-col gap-2 p-4 mt-5 rounded-md bg-[#ededed]"
+        >
           <div className="flex justify-between">
             <label htmlFor={job.id}>
               Job {`#${jobNumber}`} - [{jobDetailType}]
             </label>
             <Button
-              className="inline-block bg-red-500 px-3 py-2 text-white text-xs"
+              className="inline-block bg-red-500 px-3 py-2 text-white text-xs hover:bg-red-700 shadow-lg"
               aria-label={`Remove job ${jobNumber}`}
               onClick={() => removeJobDetail(job.id)}
             >
               Remove
             </Button>
           </div>
-          <small className="text-gray-500">
+          <small className="text-gray-500 ">
             (id:{" "}
-            <span className="px-2 bg-gray-500 text-white rounded-md">
-              {job.id}
-            </span>
-            )
+            <span className="bg-gray-500 text-white rounded-md">{job.id}</span>)
           </small>
           {"link" in job && (
             <input
@@ -96,6 +96,7 @@ const AddJobs = ({ numMaxJobs = 1 }: AddJobsProps) => {
           )}
           {"description" in job && (
             <textarea
+              className="px-3 py-4 rounded-md mt-6"
               required
               name={inputName}
               onChange={handleClearOutdatedResults}
@@ -159,12 +160,12 @@ const AddJobs = ({ numMaxJobs = 1 }: AddJobsProps) => {
           />
         </Button> */}
         <Button
-          className="text-white bg-[#302442] disabled:opacity-50"
+          className="text-white bg-[#302442]  disabled:opacity-50 mt-6 hover:shadow-lg bg-[#2D213F]"
           data-job-detail-type={JOB_DETAIL_TYPE.DESCRIPTION}
           disabled={jobs.length >= numMaxJobs}
           onClick={handleAddJobDetail}
         >
-          Add Job Descripton
+          Add Job Description
           <Image
             className="invert"
             src="/icons/file-description.svg"
@@ -174,9 +175,14 @@ const AddJobs = ({ numMaxJobs = 1 }: AddJobsProps) => {
           />
         </Button>
       </div>
-      <p>** Maximum of {numMaxJobs} job(s) allowed</p>
+      <p className="text-slate-500">
+        ** Maximum of {numMaxJobs} job(s) allowed
+      </p>
       {jobs.length ? (
-        <Button type="submit" className="text-white mt-8 bg-[#4c72f9]">
+        <Button
+          type="submit"
+          className="text-white mt-8 bg-[#4c72f9] hover:bg-[#476AE9] shadow-xl"
+        >
           Continue
         </Button>
       ) : null}
--- webapp/app/_components/job-descriptions/job-descriptions.tsx
+++ webapp/app/_components/job-descriptions/job-descriptions.tsx
@@ -9,8 +9,8 @@ const JobDescriptions = () => {
   if (!file) return null;
 
   return (
-    <section className="flex flex-col gap-12 px-32 py-10">
-      <h2 className="text-4xl font-normal leading-normal pt-14">
+    <section className="flex flex-col gap-12 px-32 py-8">
+      <h2 className="text-4xl font-normal leading-normal pt-8">
         Job Descriptions
       </h2>
       <div className="flex flex-col gap-4 text-black rounded-md p-8 bg-[#FFF5F5]">
