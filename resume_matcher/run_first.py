5aa984c51192aa8229d54836944a20b9d1870cc0
kxchourouk <chouroukkhalfi@gmail.com>
2024-07-04 12:51:33 +0100

    added a bit pf explanation to the functions

--- resume_matcher/run_first.py
+++ resume_matcher/run_first.py
@@ -17,6 +17,12 @@ logger = logging.getLogger(__name__)
 
 
 def remove_old_files(files_path):
+    """
+    Remove all files from a specified directory.
+
+    Args:
+        files_path (str): The directory path from which to delete files.
+    """
     for filename in os.listdir(files_path):
         try:
             file_path = os.path.join(files_path, filename)
@@ -30,6 +36,14 @@ def remove_old_files(files_path):
 
 
 def process_files(data_path, processed_path, file_type):
+    """
+    Process files of a specific type (resume or job description) from a source directory.
+
+    Args:
+        data_path (str): The path to the directory containing raw files.
+        processed_path (str): The path to the directory where processed files will be stored.
+        file_type (str): Type of files being processed ("resume" or "job_description").
+    """
     print(f"Processing {file_type}s from {data_path}")
     logging.info(f"Started to read from {data_path}")
     try:
@@ -45,14 +59,21 @@ def process_files(data_path, processed_path, file_type):
         exit(1)
 
     logging.info(f"Started parsing the {file_type}s.")
+ # Process each file using a progress bar
+
     for file in tqdm(file_names):
         processor_object = Processor(file, file_type)
         success = processor_object.process()
     print(f"Processing of {file_type}s is now complete.")
     logging.info(f"Parsing of the {file_type}s is now complete.")
 
+    
+
 
 def run_first():
+    """
+    Execute initial processing of resumes and job descriptions.
+    """
     process_files(RESUMES_PATH, PROCESSED_RESUMES_PATH, "resume")
     process_files(
         JOB_DESCRIPTIONS_PATH, PROCESSED_JOB_DESCRIPTIONS_PATH, "job_description"
