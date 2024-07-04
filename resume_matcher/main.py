024b450c0d704b466a20359702ce00bd6d49e273
kxchourouk <chouroukkhalfi@gmail.com>
2024-07-04 12:46:05 +0100

    added a couple of comments to the main.py file

--- resume_matcher/main.py
+++ resume_matcher/main.py
@@ -18,10 +18,26 @@ PROCESSED_JOB_DESCRIPTIONS_PATH = os.path.join(
 
 
 def get_filenames_from_dir(directory):
+    """
+    Retrieves a list of filenames from a specified directory.
+
+    Args:
+        directory (str): The directory path from which filenames are to be retrieved.
+
+    Returns:
+        list: A list of filenames (strings) found in the specified directory.
+    """
     return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
 
 
 def process_files(resume, job_description):
+    """
+    Processes a pair of resume and job description files.
+
+    Args:
+        resume (str): The filename of the resume JSON file to process.
+        job_description (str): The filename of the job description JSON file to process.
+    """
     resume_dict = read_json(PROCESSED_RESUMES_PATH + resume)
     job_dict = read_json(PROCESSED_JOB_DESCRIPTIONS_PATH + job_description)
     resume_keywords = resume_dict["extracted_keywords"]
