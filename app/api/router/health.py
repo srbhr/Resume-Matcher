aaca47af7534970c1d20e7b19a5ec493172e5a2d
Rahul Veer Singh <singh.rahul220499@gmail.com>
2025-04-26 17:23:36 +0000

    ISSUE-322 | async database layer

--- app/api/router/health.py
+++ app/api/router/health.py
@@ -1,20 +1,20 @@
 from sqlalchemy import text
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, status, Depends
 
-from app.core import get_sync_db_session
+from app.core import get_db_session
 
 health_check = APIRouter()
 
 
-@health_check.get("/ping", tags=["health_check"], status_code=status.HTTP_200_OK)
-def ping(db: Session = Depends(get_sync_db_session)):
+@health_check.get("/ping", tags=["Health check"], status_code=status.HTTP_200_OK)
+async def ping(db: AsyncSession = Depends(get_db_session)):
     """
     health check endpoint
     """
     try:
-        result = db.execute(text("SELECT 1")).fetchone()
-        db_status = "reachable" if result is not None else "not reachable"
+        result = await db.execute(text("SELECT 1"))
+        db_status = "reachable" if result.fetchone() is not None else "not reachable"
     except Exception as e:
         db_status = f"error: {str(e)}"
     return {"message": "pong", "database": db_status}
