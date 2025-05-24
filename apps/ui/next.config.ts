2311e9e5120602821baa535ec58becde914b256e
srbhr <srbh077@gmail.com>
2025-05-25 01:57:23 +0530

    Updated the file structure to include

---
+++ apps/ui/next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+    async rewrites() {
+    return [
+      {
+        source: '/api_be/:path*',
+        destination: 'http://localhost:8000/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
