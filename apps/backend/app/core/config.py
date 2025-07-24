9f0bf6f02bfac2fb11ed2aed91d1fdd54a5124bd
Arsh <87128542+ArshCypherZ@users.noreply.github.com>
2025-07-24 17:56:25 +0530

    Update apps/backend/app/core/config.py
    Co-authored-by: Rahul Veer Singh <singh.rahul220499@gmail.com>

--- apps/backend/app/core/config.py
+++ apps/backend/app/core/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
     SESSION_SECRET_KEY: Optional[str]
     DB_ECHO: bool = False
     PYTHONDONTWRITEBYTECODE: int = 1
-    openai_api_key: Optional[str] = None
+    OPENAI_API_KEY: Optional[str] = None
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, ".env"),
